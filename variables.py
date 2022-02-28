4dbbf21ec30db0c780019005678fa2f2557315f4
kiou <quentin.far@gmail.com>
2022-02-28 16:02:35 +0100

    Création du repo
    ajout des premiers chapitres de cours.
    ajout des exos correpsondants.

---
+++ variables.py
@@ -0,0 +1,38 @@
+# ceci est un commentaire
+
+# foo
+# bar 
+# bazz
+nombre = 123.0
+print(nombre)
+
+nombre = 3.14
+print(nombre)
+
+nombre = "123"
+print(nombre)
+
+text = "du texte"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_sugar = False
+print(has_sugar)
+
+has_accepted_ula = None
+print(has_accepted_ula)
+
+html_code = '<h1>titre de mon blog</h1>'
+print(html_code)
+nickname = "John \"Dead\" Doe"
+print(nickname)
+
+multiline_text = "foo\n baz\n bar "
+print(multiline_text)
+
+multiline_text = """foo
+'baz'
+bar"""
+print(multiline_text)
