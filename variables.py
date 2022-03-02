5749d36bfc5e7c3ea8f87f86f20822a589fec3e3
kiou <quentin.far@gmail.com>
2022-03-02 11:58:23 +0100

    Mise à jours cours variables exploration de conversions de type

--- variables.py
+++ variables.py
@@ -3,8 +3,9 @@
 # foo
 # bar 
 # bazz
-nombre = 123.0
+nombre = 123
 print(nombre)
+print(type(nombre))
 
 nombre = 3.14
 print(nombre)
@@ -20,14 +21,17 @@ print(is_day)
 
 has_sugar = False
 print(has_sugar)
+print(type(has_sugar))
 
 has_accepted_ula = None
 print(has_accepted_ula)
+print(type(has_accepted_ula))
 
 html_code = '<h1>titre de mon blog</h1>'
 print(html_code)
 nickname = "John \"Dead\" Doe"
 print(nickname)
+print(type(nickname))
 
 multiline_text = "foo\n baz\n bar "
 print(multiline_text)
@@ -39,8 +43,35 @@ print(multiline_text)
 
 nombre6 = 3.14
 print(type(nombre6))
-print(nombre6)
 
-nombre6 = int(nombre6)
-print(type(nombre6))
-print(nombre6)
+
+
+nombre = '5'
+print(type(nombre))
+nombre = int(nombre)
+print(type(nombre))
+
+nombre = '5'
+print(type(nombre))
+nombre = float(nombre)
+print(type(nombre))
+
+nombre = 3.14
+print(nombre)
+print(type(nombre))
+nombre = int(nombre)
+print(nombre)
+
+nombre = str(nombre)
+print(type(nombre))
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+
+my_var = 0
+my_var = bool(my_var)
+print(my_var)
+my_var = [132 , 'abc', False]
+my_var = bool(my_var)
+print(my_var)
